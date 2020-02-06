f9bde2075cf45636ac87b1504d60fe4d458a9899
Gianluca Guarini <gianluca.guarini@gmail.com>
2020-02-06 20:38:01 +0100

    updated: use internal cache for the loaded components

--- index.next.js
+++ index.next.js
@@ -1,31 +1,33 @@
 import { component, pure } from 'riot'
 
+// this object will contain all the components implementations lazy loaded
+const cache = new WeakMap()
+
+// expose the cache as static property
+lazy.cache = cache
+
 export default function lazy(Loader, Component) {
   // it could be that the user don't want to use a loader for whatever reason
   const hasLoader = Loader && Component
   const LazyComponent = hasLoader ? Component : Loader
   const load = () => typeof LazyComponent === 'function' ? LazyComponent() : Promise.resolve(LazyComponent)
-  // cache the lazy loaded component
-  const cache = {
-    component: null
-  }
+  const cachedComponent = cache.get(LazyComponent)
 
   return {
     name: 'lazy',
     exports: pure(({ slots, attributes, props }) => ({
       mount(el, parentScope) {
-
         this.el = el
         this.isMounted = true
         const mount = () => this.mountLazyComponent(parentScope)
 
-        if (cache.component) {
+        if (cachedComponent) {
           mount()
         } else {
           if (hasLoader) this.createManagedComponent(Loader, parentScope)
 
           load().then(data => {
-            cache.component = data.default || data
+            cache.set(LazyComponent, data.default || data)
             mount()
           })
         }
@@ -44,7 +46,7 @@ export default function lazy(Loader, Component) {
         if (this.component) this.component.unmount(true)
 
         // replace the old component instance with the new lazy loaded component
-        this.createManagedComponent(cache.component, parentScope)
+        this.createManagedComponent(cache.get(LazyComponent), parentScope)
       },
       update(parentScope) {
         if (this.isMounted && this.component) this.component.update({}, parentScope)
